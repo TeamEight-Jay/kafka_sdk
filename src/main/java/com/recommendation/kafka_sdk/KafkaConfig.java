56cd8b7974c9fc232de8a3fa9675a277ddf06ece
jayjoshi <jay.joshi@coviam.com>
2019-01-28 19:09:10 +0530

    add kafka config

--- src/main/java/com/recommendation/kafka_sdk/KafkaConfig.java
+++ src/main/java/com/recommendation/kafka_sdk/KafkaConfig.java
@@ -1,6 +1,7 @@
 package com.recommendation.kafka_sdk;
 
 import com.recommendation.kafka_sdk.contest.PlayQuestionKafkaMessage;
+import com.recommendation.kafka_sdk.contest.SubscribeContestKafkaMessage;
 import com.recommendation.kafka_sdk.socialnetwork.FollowKafkaMessage;
 import com.recommendation.kafka_sdk.socialnetwork.LikeKafkaMessage;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -32,7 +33,7 @@ public class KafkaConfig {
 
 
     @Bean
-    public KafkaTemplate<String, PlayQuestionKafkaMessage> kafkaTemplate() {
+    public KafkaTemplate<String, PlayQuestionKafkaMessage> playQuestionKafkaTemplate() {
         return new KafkaTemplate<>(playQuestionFactory());
     }
 
@@ -70,7 +71,22 @@ public class KafkaConfig {
         return new KafkaTemplate<>(likeFactory());
     }
 
+    @Bean
+    public ProducerFactory<String, SubscribeContestKafkaMessage> subscribeContestFactory() {
+        Map<String, Object> config = new HashMap<>();
 
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+
+    @Bean
+    public KafkaTemplate<String, SubscribeContestKafkaMessage> subscribeContestKafkaTemplate() {
+        return new KafkaTemplate<>(subscribeContestFactory());
+    }
 
 
 
