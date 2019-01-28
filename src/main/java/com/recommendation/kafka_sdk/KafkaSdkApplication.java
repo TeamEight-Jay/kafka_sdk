5c5f12221a3a13e0ef00bb060c66c792ca29a11f
jayjoshi <jay.joshi@coviam.com>
2019-01-28 18:17:37 +0530

    Initial commit

---
+++ src/main/java/com/recommendation/kafka_sdk/KafkaSdkApplication.java
@@ -0,0 +1,14 @@
+package com.recommendation.kafka_sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSdkApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/recommendation/kafka_sdk/KafkaSdkApplicationTests.java
@@ -0,0 +1,17 @@
+package com.recommendation.kafka_sdk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaSdkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
